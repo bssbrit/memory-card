a2ef4aa395718c543aa9ed23a8095cd01583906b
bssbrit <brunosabrito2@gmail.com>
2023-11-02 16:00:31 -0300

    learning how to call an api in react kkkk

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemonName, setPokemon] = useState([])
+  let counter = 0
+   useEffect(() => {
+    const getInfo = function () {fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
+    .then(res => res.json())
+    .then(json =>  fetch(`${json.results[0].url}`)
+    .then(response => response.json())
+    .then(data => setPokemon(oldArray => [...oldArray, data])) 
+      )
+    .catch(error => console.log(error))
+    }
+    getInfo()
+    return () => {
+      clearInterval(getInfo);
+    }
+  
 
+  }, [])
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div >
+      <button onClick={() => console.log(pokemonName)}>poke</button>
+     {pokemonName.map(poke => (
+        <p key={poke.name}>{poke.name}</p>
+      ))
+        
+      } 
+    </div>
   )
 }
 
 export default App
+/* const [pokemonName, setPokemon] = useState([])
+  
+   useEffect(() => {
+    const getInfo = function () {fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
+    .then(res => res.json())
+    .then(json => fetch(`${json.results[0].url}`)
+    .then(response => response.json())
+    .then(data => setPokemon(oldArray => [...oldArray, data])) 
+      )
+    .catch(error => console.log(error))
+    }
+    getInfo()
+    return () => {
+      clearInterval(getInfo);
+    }
+  
+
+  }, [])
+   */
