bae7cd39afaea817d211f918aeeffd6bf9a703c2
bssbrit <brunosabrito2@gmail.com>
2023-11-02 22:08:08 -0300

    having difficulties fetching data kkkk

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import { useEffect, useState } from 'react'
 
 
 function App() {
-  const [pokemonName, setPokemon] = useState([])
-  let counter = 0
+  const [pokemonName, setPokemon] = useState(null)
+  const [pokeStats, setStats] = useState([])
    useEffect(() => {
     const getInfo = function () {fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
     .then(res => res.json())
-    .then(json =>  fetch(`${json.results[0].url}`)
-    .then(response => response.json())
-    .then(data => setPokemon(oldArray => [...oldArray, data])) 
-      )
+    .then(data =>  setPokemon(data.results))
+    .then(() => {
+      pokemonName.forEach(element => {
+        console.log(element.name)
+        fetch(`${element.url}`)
+        .then(res => res.json())
+        .then(data => {
+           setStats([...pokeStats, data])
+          console.log(data)
+        })
+        .catch(error => console.log(error))
+      });
+    })
     .catch(error => console.log(error))
     }
     getInfo()
@@ -24,11 +33,9 @@ function App() {
   return (
     <div >
       <button onClick={() => console.log(pokemonName)}>poke</button>
-     {pokemonName.map(poke => (
-        <p key={poke.name}>{poke.name}</p>
-      ))
+      <button onClick={() => console.log(pokeStats)}>stats</button>
         
-      } 
+       
     </div>
   )
 }
