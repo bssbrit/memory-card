ae8cdeafdb2c896285bcf137a94b6ae9a0f4ec04
bssbrit <brunosabrito2@gmail.com>
2023-11-03 22:24:46 -0300

    done with logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react'
 import Pokemon from './components/Pokemon'
+//import { v4 as uuid } from "uuid";
 function App() {
   const [pokemonName, setPokemon] = useState([])
-  /* 
-   */
+  
+ 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=10')
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=30')
     .then(response => {
       const responseJson = response.json();
       return responseJson
@@ -15,23 +16,42 @@ function App() {
       let pokemonStats = []
       for (const pokemon of pokemons) {
         pokemon.data = await fetch(pokemon.url).then((res) => res.json());
-        
+        pokemon.data.status = false /* Para poder identificar quais foram clicadas */
         pokemonStats.push(pokemon.data)
         
       }
-      setPokemon(pokemonStats)
-         
+      setPokemon(shuffleArray(pokemonStats))
       
     })
-
    
+    
 
   }, [])
+   
+  const restartPokes = function () {
+    console.log("hi")
+    const newArr = pokemonName.map((poke) => {
+      return { ...poke, status:false}
+    })
+    setPokemon(newArr) 
+   
+  }
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      // Generate a random index between 0 and i (inclusive)
+      const j = Math.floor(Math.random() * (i + 1));
   
+      // Swap elements at i and j
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+
   return (
-    <div >
-      <button onClick={() => console.log(pokemonName)}>poke</button>
-      <Pokemon pokemonName={pokemonName}/>
+    <div className='container'>{/* 
+      <button onClick={() => console.log(pokemonName)}>poke</button> */}
+      <Pokemon pokemonName={pokemonName} restartPokes={restartPokes} shuffleArray={shuffleArray}/>
     </div>
   )
 }
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,22 +1,48 @@
 import PropTypes from 'prop-types';
-const Pokemon = ({pokemonName}) => {
+import { useState } from 'react';
+const Pokemon = ({pokemonName, restartPokes, shuffleArray}) => {
+  const [roundCounter, setCounter] = useState(0)
+  const [record, setRecord] = useState(0)
+  const checkClick = function(poke) {
+   
+   if(poke.status){ 
+    console.log("Esse botão ja foi clicado");
+    shuffleArray(pokemonName)
+    restartPokes()
+     
+    if(roundCounter > record)setRecord(roundCounter)
+    setCounter(0)
+  /* e dai um função de reiniciar o jogo */}
+   else {
+  console.log("Botão foi clicado");
+  poke.status = true 
+  setCounter(roundCounter + 1)
+  }
+  }
   return (
-    <div className='pokePhotos'>
+    <div className='pokeTabuleiro'>
+      <p>Contador do round {roundCounter}</p>
+      <p>Best Score {record}</p>
+    <div className='pokeDiv' >
+      
       {pokemonName.map(poke => (
         
-        <div key={poke.name}>
-          <img src={poke.sprites.front_default} alt="" />
+        <div className='pokeCard' key={poke.name} onClick={() => checkClick(poke)}>
+          <img src={poke.sprites.front_default} alt=""  />
           <p style={{
             textTransform:'uppercase'
           }}>{poke.name}</p>
         </div>
       ))}
     </div>
+    </div>
   )
 }
 
 Pokemon.propTypes = {
   pokemonName: PropTypes.array,
+  restartPokes: PropTypes.func,
+  shuffleArray: PropTypes.func
 };
 
 export default Pokemon
